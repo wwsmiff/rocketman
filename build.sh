30b72f1d9fe122f53558ac62f87446f7d92f1bd3
wwsmiff <arnav0872@gmail.com>
2024-04-28 00:53:37 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,10 +3,18 @@
 set -xe
 
 CC="g++"
-CFLAGS="-Wall -std=c++20 -O2 -o"
+CFLAGS="-Wall -std=c++20 -O2"
 LFLAGS="-lsfml-window -lsfml-system -lsfml-graphics -lm"
 EXE="rocketman"
 SRC="main.cpp random.cpp"
 INCLUDEDIR=""
 
-$CC $CFLAGS $SRC -o $EXE $LFLAGS
+build() {
+  $CC $CFLAGS $SRC -o $EXE $LFLAGS
+}
+
+clean() {
+  rm $EXE
+}
+
+$1 $2 $3 $4 $5
